1a0ac74e29cbd7ad18f5178b4d60e55aa80937e7
VincenzoZ24 <zitov287@gmail.com>
2022-10-20 14:34:46 +0200

    limitato prezzo a 2 cifre decimali

--- script.js
+++ script.js
@@ -15,12 +15,14 @@ let prezzoBiglietto = kmDaFare * "0.21";
 console.log("prezzo biglietto", prezzoBiglietto , "euro");
 if (eta < 18) {
     let sconto1 = (prezzoBiglietto * 0.2);
-    let prezzoScontato1 = (prezzoBiglietto - sconto1);
-    console.log("sconto minorenni ", prezzoScontato1);
+    let prezzo1 = (prezzoBiglietto - sconto1);
+    let prezzoScontato1 = prezzo1.toFixed(2);
+    console.log("sconto minorenni ", prezzoScontato1, "euro");
 }
 else if (eta > 65) {
     let sconto2 = (prezzoBiglietto * 0.4);
-    let prezzoScontato2 = (prezzoBiglietto - sconto2);
-    console.log("sconto over 65", prezzoScontato2);
+    let prezzo2 =(prezzoBiglietto - sconto2);
+    let prezzoScontato2 = prezzo2.toFixed(2);
+    console.log("sconto over 65", prezzoScontato2, "euro");
     
 }
